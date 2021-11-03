8192b11fc4c69766f6f3af150c68db9071cfe765
Jelmer Prins <jelmer@pageon.be>
2021-11-03 19:51:03 +0100

    Solve twig loading issues

--- src/DependencyInjection/PageonDoctrineDataGridExtension.php
+++ src/DependencyInjection/PageonDoctrineDataGridExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class PageonDoctrineDataGridBundleExtension extends Extension
+class PageonDoctrineDataGridExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
--- src/PageonDoctrineDataGridBundle.php
+++ src/PageonDoctrineDataGridBundle.php
@@ -2,14 +2,14 @@
 
 namespace Pageon\DoctrineDataGridBundle;
 
-use Pageon\DoctrineDataGridBundle\DependencyInjection\PageonDoctrineDataGridBundleExtension;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Pageon\DoctrineDataGridBundle\DependencyInjection\TwigExtensionPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class PageonDoctrineDataGridBundle extends Bundle
 {
-    public function getContainerExtension(): ExtensionInterface
+    public function build(ContainerBuilder $container): void
     {
-        return new PageonDoctrineDataGridBundleExtension();
+        $container->addCompilerPass(new TwigExtensionPass());
     }
 }
